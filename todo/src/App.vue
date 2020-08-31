3906c0d958d435d513404376ed3ad67cbad82136
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-08-31 14:54:19 -0500

    centering card

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,75 +1,83 @@
 <template>
-  <div id="app">
-    <Header />
-    <AddTodo v-on:add-todo="addTodo"/>
-   <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-  </div>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <Header />
+              <AddTodo v-on:add-todo="addTodo" />
+              <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
-import Todos from './components/Todos'
-import Header from './components/layout/Header'
-import AddTodo from './components/AddTodo'
-
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Todos,
     Header,
-    AddTodo
+    AddTodo,
   },
-  data(){
-    return{
+  data() {
+    return {
       todos: [
         {
           id: 1,
           title: "Todo One",
-          completed: false
+          completed: false,
         },
         {
           id: 2,
           title: "Todo Two",
-          completed: false
+          completed: false,
         },
         {
           id: 3,
           title: "Todo Three",
-          completed: false
-        }
-      ]
-    }
+          completed: false,
+        },
+      ],
+    };
   },
   methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
     },
-    addTodo(newTodo){
+    addTodo(newTodo) {
       this.todos = [...this.todos, newTodo];
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body{
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover{
-    background: #666;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
--- todo/src/components/Todos.vue
+++ todo/src/components/Todos.vue
@@ -1,9 +1,11 @@
 <template>
+
     <div>
         <div v-bind:key="todo.id" v-for="todo in todos"> 
             <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
         </div>
     </div>
+ 
 </template>
 
 <script>
